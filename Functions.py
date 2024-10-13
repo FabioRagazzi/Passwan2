f2d548c37805c66dc829433572aa6367df4bf30d
FabioRagazzi <fabio.ragazzi.98@gmail.com>
2024-10-13 15:16:29 +0200

    Basically working but ugly design

---
+++ Functions.py
@@ -0,0 +1,66 @@
+import json
+import os
+
+
+def increment_string(num_str):
+    num = int(num_str)+1
+    return str(num % 10)
+
+
+def read_json_file(file_path, default_value):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)  # Parse JSON data
+    except json.JSONDecodeError:
+        data = default_value
+    except FileNotFoundError:
+        data = default_value
+        with open(file_path, 'w') as file:
+            pass
+    return data
+
+
+def write_json_file(file_path, data):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+def id_num_to_str(n):
+    id_num_str = str(n)
+    return id_num_str.zfill(3)
+
+
+def text_to_state(text):
+    # initializing output
+    output_state = [[0, 0, 0, 0],
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0],
+                    [0, 0, 0, 0]]
+
+    # converting text to ASCII value
+    for j in range(4):
+        for i in range(4):
+            output_state[i][j] = ord(text[4 * j + i])
+
+    return output_state
+
+
+def state_to_text(state_in, char_list):
+    # initializing output
+    text_out = ""
+
+    # converting to text using only the characters
+    # present in char_list
+    list_length = len(char_list)
+    for j in range(4):
+        for i in range(4):
+            text_out += char_list[state_in[i][j] % list_length]
+
+    return text_out
+
+
+def add_to_clipboard(text_to_add):
+    with open("temp.txt", "w") as file:
+        file.write(text_to_add.strip())
+    os.system("clip < temp.txt")
+    os.remove("temp.txt")
